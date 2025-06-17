0f228d1854adffd1634c44ebf3691af994ae788e
Oyindamola1111 <ajalajohnson27@gmail.com>
2025-06-17 15:19:15 +0100

    new commit

---
+++ script.js
@@ -0,0 +1,138 @@
+// to change the content in your html from javaScript
+// let man=document.getElementById('counting')
+// console.log(man);
+
+
+// Ways to make adiition,subtractions,mut,div and others
+// let count=4+1
+// console.log(count);  addition
+
+// let count = 5*2
+// console.log(count);    mut
+
+//let count = 10 - 5
+// console.log(count);     subtraction
+
+//let count = 6/2
+//console.log(count);     division
+
+
+
+
+// CALLING A FUNCTION
+
+//  let lap1 = 40
+//  let lap2= 10
+//  let lap3 = 30
+// function addition() {
+//     let totalSum = lap1+lap2+lap3
+//     console.log(totalSum);
+//     console.log(lap1+lap2+lap3);
+      
+// }addition()
+
+// function addition() {
+//     let lap1 = 40
+//     let lap2 =10
+//     let lap3 =30
+//     let sum =lap1+lap2+lap3
+//     console.log(sum);
+    
+// }addition()          can also be donethiws way
+
+
+
+
+// how to make increment
+
+// let kipCounting = document.getElementById('counting')
+// console.log(kipCounting);
+
+// let count=0
+// console.log(count);
+
+
+// function addition() {
+//    count=count+1
+//    kipCounting.innerText=count;
+// }addition()
+
+
+
+// let bonusPoint = 50 
+// console.log(bonusPoint);
+// bonusPoint=bonusPoint+50
+// console.log(bonusPoint);
+// bonusPoint=bonusPoint-75
+// console.log(bonusPoint);
+// bonusPoint=bonusPoint+45
+// console.log(bonusPoint);
+
+// function calling() {
+//     console.log(42);
+    
+// }calling()
+
+// let lap1 = 10
+// let lap2 =5
+// let lap3 = 18
+// let lap4 = 20
+// function lapAddition() {
+//     console.log(lap1+lap2+lap3+lap4);
+    
+// }lapAddition()
+
+
+// let lapCompleted = 0
+// function increment() {
+//     lapCompleted=lapCompleted+1
+// }increment()
+// increment()
+// increment()
+// console.log(lapCompleted);
+
+// let count = 0
+// console.log(count);
+// let addition=document.getElementById('counting')
+// console.log(addition);
+
+// function increment() {
+//     count=count+1
+//     addition.innerText=count
+// }increment()
+
+
+// function save() {
+//     console.log(count);
+    
+// }save()
+
+
+// let userName = "John"
+// let message = "you have three new notifications"
+// let messageToUser= message +","+ userName +"!";
+// console.log(messageToUser);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

35d9878a9aeab5e2fe98ce95cfd1669a4ddf42ae
Oyindamola1111 <ajalajohnson27@gmail.com>
2025-06-25 15:27:17 +0100

    new things added

--- script.js
+++ script.js
@@ -1,119 +1,105 @@
-// to change the content in your html from javaScript
-// let man=document.getElementById('counting')
-// console.log(man);
 
+                      // BUILD A PASSENGER COUNTER APP 
 
-// Ways to make adiition,subtractions,mut,div and others
-// let count=4+1
-// console.log(count);  addition
-
-// let count = 5*2
-// console.log(count);    mut
-
-//let count = 10 - 5
-// console.log(count);     subtraction
-
-//let count = 6/2
-//console.log(count);     division
-
-
-
-
-// CALLING A FUNCTION
-
-//  let lap1 = 40
-//  let lap2= 10
-//  let lap3 = 30
-// function addition() {
-//     let totalSum = lap1+lap2+lap3
-//     console.log(totalSum);
-//     console.log(lap1+lap2+lap3);
-      
-// }addition()
-
-// function addition() {
-//     let lap1 = 40
-//     let lap2 =10
-//     let lap3 =30
-//     let sum =lap1+lap2+lap3
-//     console.log(sum);
-    
-// }addition()          can also be donethiws way
-
-
-
-
-// how to make increment
-
-// let kipCounting = document.getElementById('counting')
-// console.log(kipCounting);
-
-// let count=0
-// console.log(count);
-
-
-// function addition() {
-//    count=count+1
-//    kipCounting.innerText=count;
-// }addition()
-
-
-
-// let bonusPoint = 50 
-// console.log(bonusPoint);
-// bonusPoint=bonusPoint+50
-// console.log(bonusPoint);
-// bonusPoint=bonusPoint-75
-// console.log(bonusPoint);
-// bonusPoint=bonusPoint+45
-// console.log(bonusPoint);
-
-// function calling() {
-//     console.log(42);
-    
-// }calling()
-
-// let lap1 = 10
-// let lap2 =5
-// let lap3 = 18
-// let lap4 = 20
-// function lapAddition() {
-//     console.log(lap1+lap2+lap3+lap4);
-    
-// }lapAddition()
-
-
-// let lapCompleted = 0
-// function increment() {
-//     lapCompleted=lapCompleted+1
-// }increment()
-// increment()
-// increment()
-// console.log(lapCompleted);
-
+// let saveEl = document.getElementById("save-el")
+// let plus = document.getElementById("counting")
 // let count = 0
-// console.log(count);
-// let addition=document.getElementById('counting')
-// console.log(addition);
 
 // function increment() {
 //     count=count+1
-//     addition.innerText=count
-// }increment()
-
+//     plus.textContent=count
+// }
 
 // function save() {
-//     console.log(count);
-    
-// }save()
+//     let dashCount = count + " - "
+//    saveEl.textContent=saveEl.textContent+dashCount
+//    plus.textContent=0
+//    count = 0
+// }
 
 
-// let userName = "John"
-// let message = "you have three new notifications"
-// let messageToUser= message +","+ userName +"!";
-// console.log(messageToUser);
+
+// THROW A NEW PARAGRAPH EROR ON A PURCHASE BUTTON WHEN CLICKED
+
+// let shoe = document.getElementById("error")
+// function buyError() {
+//     shoe.innerText="please something went wrong, try again"
+//     shoe.style.color="red"
+// }
 
 
+
+                        // BUILD A CALCULATOR
+
+// let num1 =8
+// let num2 = 2
+// let total = document.getElementById("sum-el")
+
+// document.getElementById("num1-el").textContent=num1
+// document.getElementById("num2-el").textContent=num2 
+
+// function Addition() {
+//     let adding =num1+num2
+// total.textContent="Sum: " + adding
+
+// }
+
+
+// function subtract() {
+//     let adding =num1-num2
+// total.textContent="Sum: " + adding
+
+// }
+
+// function divide(){
+//     let adding=num1/num2 
+//     total.textContent="sum:" +adding
+// }
+
+// function multiply() {
+//     let adding=num1*num2
+//     total.textContent="sum: "+ adding
+// }
+
+
+// BUILD A BLACKJACK GAME
+//random number between 2-11
+// let firstCard=4
+// let secondCard=8
+// let total= firstCard+secondCard
+// console.log(total);
+
+// if (total< 21) {
+//     console.log("you have earned a new card");
+// }else if (total === 21) {
+//     console.log("yeeaaahhh!!! you Won");
+// }else if(total > 21) {
+//  console.log("Sorry you lost!");
+// }
+
+
+// let age=21 
+// if (age < 21) {
+//     console.log("you can not enter the club");
+// }else{
+//     console.log("Welcome!");
+// }
+
+
+
+// let input=document.getElementById('box');
+// let appear=document.getElementById('para');
+
+// function ageRange() {
+// if (input.value <= 99) {
+//   appear.textContent = "not eligible";
+// }else if (input.value ==100) {
+//    appear.textContent = "Here is your birthday card from the king 😎";
+//    appear.style.color="green"
+// } else {
+//     appear.textContent="Step away, You have gotten one card already";
+// }
+// }
 
 
 

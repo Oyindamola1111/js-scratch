0ff0a2a414100e10b00d6667dc3dbfbfd4ba9072
Oyindamola1111 <ajalajohnson27@gmail.com>
2025-07-09 14:32:55 +0100

    new things

--- script.js
+++ script.js
@@ -63,6 +63,7 @@
 
 
 // BUILD A BLACKJACK GAME
+
 //random number between 2-11
 // let firstCard=4
 // let secondCard=8
@@ -87,21 +88,102 @@
 
 
 
+
+// PROJECT TO CHECK IF AGE 100 IS ELIGIBLE FOR KINGS CARD
+
+
 // let input=document.getElementById('box');
 // let appear=document.getElementById('para');
 
 // function ageRange() {
-// if (input.value <= 99) {
-//   appear.textContent = "not eligible";
-// }else if (input.value ==100) {
+
+// const age = parseInt(input.value);
+
+// if (isNaN(age)) {
+//   appear.textContent = "enter a valid number";
+// }else if (age === 100) {
 //    appear.textContent = "Here is your birthday card from the king 😎";
 //    appear.style.color="green"
-// } else {
-//     appear.textContent="Step away, You have gotten one card already";
+// } else if(age<100){
+//    appear.textContent="you are not eligible"
+//    appear.style.color="red"
+// }else if(age>100){
+//    appear.textContent="you have gotten a card already"
 // }
 // }
 
 
+
+let firstCard= 10
+let secondCard=11
+let cardArray =[firstCard,secondCard]
+let sum = firstCard+secondCard
+let hasBlackJack=false
+let isAlive = true
+let message = ""
+let appear= document.getElementById("display")
+let show=document.getElementById("again")
+let sumEl=document.getElementById("sum-el")
+let cardEl=document.getElementById("card-el")
+
+function start() {
+    render()
+}
+
+
+function render() {
+    cardEl.textContent="Card: " + cardArray[0]+ " " +cardArray[1] 
+    sumEl.textContent="sum: "+ sum
+   if (sum <= 20) {
+ message = "do you want to draw a new card"
+ 
+}else if (sum === 21) {
+ message = "wohoo! you have got a blackjack"
+ 
+ hasBlackJack=true
+}else{
+    message="you are out of the game"
+    isAlive=false
+}
+
+appear.textContent=message
+show.textContent=message
+}
+
+function start() {
+    let card=6
+    sum +=card
+    cardArray.push(card)
+    console.log(cardArray);
+    
+}
+
+// let experience = ["sail cohort 5","no other experience"]
+// console.log(experience[1]);
+// console.log(experience[0]);
+
+
+// let education = ["uni ilorin","barachel model college"]
+// console.log(education[1]);
+// console.log(education[0]);
+
+
+          // ARRAYS
+// let licenses= ["none for now", "goin for one soon"]
+// let skill=["football", "tenis","coding"]
+// console.log(skill[2]);
+// console.log(skill[1]);
+// console.log(skill[0]);
+
+// let myself = ["sanusi oyindamola", 30, true]
+// let newMessage="same here"
+// myself.pop()
+// console.log(myself);
+
+
+
+ 
+ 
 
 
 
